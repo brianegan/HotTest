c5cd1aa10876d136ff3aabf6c23b460f1dcf8201
Brian Egan <brian@brianegan.com>
2010-09-24 11:01:21 -0700

    Shifting files around for the public view served by express

--- app.js
+++ app.js
@@ -5,21 +5,18 @@ var express = require('express');
 var io = require('socket.io');
 var http = require('http');
 
-
 /*
  * Express Base =============================================================
  *
  * Used to handle requests and serve our public files
  */
   
-
 //Create our App
 var app = express.createServer();
 
 //Set App properties
 app.set('views', __dirname + '/views');
 app.use(express.staticProvider(__dirname + '/public'));
-
 
 /*
  * =Routing
@@ -42,8 +39,5 @@ app.get('/', function(req, res){
  *
  * Attach all our goodies to an actual port =)
  */
-  
-
-// Listeners on port 3000
 app.listen(3000);
 var socket = io.listen(app);
