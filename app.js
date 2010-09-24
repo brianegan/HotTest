bd5587874cb388dfa8c22fac39b9ce8e1336fe89
Brian Egan <brian@brianegan.com>
2010-09-24 11:41:05 -0700

    Refactoring app.js to be simpler

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 //-- Jasmine Server: A Simple Server & Client for running automated Jasmine Tests!
 
 /* Requires */
-var express = require('express');
-var io = require('socket.io');
-var http = require('http');
+var express = require('express'),
+    io = require('socket.io'),
+    http = require('http'),
+    app = express.createServer(),
+    socket = io.listen(app);
 
 /*
  * Express Base =============================================================
  *
  * Used to handle requests and serve our public files
  */
-  
-//Create our App
-var app = express.createServer();
-
-//Set App properties
-app.set('views', __dirname + '/views');
 app.use(express.staticProvider(__dirname + '/public'));
-
-/*
- * =Routing
- */
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
-
 
 /*
  * Socket.IO ================================================================
  *
  * Configures how Socket IO should behave! 
  */
+
+// accepting socket connections and broadcast events
 
 
 
@@ -40,4 +30,3 @@ app.get('/', function(req, res){
  * Attach all our goodies to an actual port =)
  */
 app.listen(3000);
-var socket = io.listen(app);
