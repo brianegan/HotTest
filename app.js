6ed4e2dc2049c915cb3d34ac9cab9c1f5e7c3231
Brian Egan <brian@brianegan.com>
2010-09-24 17:39:17 -0700

    Big changes, probably should have made far more commits. Got the test driver working in rough form. Multiple clients can connect, and when hard coded files are modified, the browsers will run the jasmine scripts and return the results.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 //-- Jasmine Server: A Simple Server & Client for running automated Jasmine Tests!
 
-/* Requires */
+// Requires
 var express = require('express'),
     io = require('socket.io'),
     http = require('http'),
+    sys = require('sys'),
+    fs = require('fs'),
+
+// App Vars 
     app = express.createServer(),
-    socket = io.listen(app),
-    sys = require('sys');
+    zombies = [], // Raaaaawwrr -- our browsers that do our testing
+    results = [];
 
 /*
  * Express Base =============================================================
@@ -20,17 +24,40 @@ app.use(express.staticProvider(__dirname + '/public'));
  *
  * Configures how Socket IO should behave! 
  */
+
+var socket = io.listen(app, {
+  log: function () { }
+});
 socket.on('connection', function(client) {
 
-  client.send('run');
-
-  client.on('message', function(message) {
-    sys.puts(message);
-  });
-
-
+});
+socket.on('clientMessage', function(message, client) {
+  switch(message) {
+    case 'newClient':
+      zombies.push(client);
+      break;
+    default:
+      var results = JSON.parse(message);
+      sys.puts(results);
+      break;
+  }
 });
 
+/*
+ * Watchers =================================================================
+ *
+ * I be watchin you, watchin me, yeah you know!
+ */
+fs.watchFile(__dirname + '/public/spec/', { interval: 5 }, function (curr, prev) {
+  if (curr.mtime.toString() === prev.mtime.toString()) {
+    return;
+  } else {
+    sys.puts("still say me");
+  }
+  for(i = 0, len = zombies.length; i < len; i++) {
+    zombies[i].send('run');
+  }
+});
 
 /*
  * Listeners ================================================================
---
+++ public/src/jasmine-client.js
@@ -0,0 +1,121 @@
+//-- jasmineClient by Brian Egan
+var jasmineClient = {
+
+  listen: function() {
+
+    // Setup our Vars
+    var socket = new io.Socket(null, {port: 3000}),
+        self = this;
+
+    // Connect to our Server
+    socket.connect();
+
+    socket.on('connect', function() {
+      socket.send('newClient');
+    });
+
+    // Run our tests when told!
+    socket.on('message', function(message) {
+      self.initRun(message);
+    });
+  },
+
+  run: function() {
+    var JsReporter = new jasmine.JsApiReporter(),
+        TrivialReporter = new jasmine.TrivialReporter(),
+        socket = new io.Socket(null, {port: 3000}),
+        sendTests = function() {
+          if (JsReporter.finished) {
+            socket.connect();
+            socket.send(JSON.stringify(JsReporter));
+          } else {
+            setTimeout(sendTests, 50);
+          }
+        };
+
+    // Hook em up to Jasmine
+    jasmine.getEnv().addReporter(TrivialReporter);
+    jasmine.getEnv().addReporter(JsReporter);
+    jasmine.getEnv().execute();
+
+    sendTests();
+
+  },
+
+  initRun: function(message) {
+    if (message === 'run') {
+      if(document.getElementById('testFrame')) { 
+        document.getElementById('testContainer').removeChild(document.getElementById('testFrame'));
+      }
+      var testFrame = '<iframe src="/run.html" id="testFrame"><p>Your browser does not support iframes.</p></iframe>';
+      document.getElementById('testContainer').innerHTML = testFrame;
+    }
+  }
+
+};
+
+
+/* 
+// Create our new Socket & connect to it
+var socket = new io.Socket(null, {port: 3000});
+socket.connect();
+
+var testEnv = jasmine.getEnv(),
+    origEnv = Object.create(jasmine.getEnv()),
+    JsReporter = {},
+    TrivialReporter = {},
+
+    clearTests = function() {
+      if (document.getElementsByTagName('div')[0]) { 
+        document.body.removeChild(document.getElementsByTagName('div')[0]);
+      }
+      testEnv = origEnv;
+      JsReporter = {};
+      TrivialReporter = {};
+    },
+    
+    runTests = function() { 
+      // Create our reporters
+      testEnv = new jasmine.getEnv();
+      JsReporter = new jasmine.JsApiReporter();
+      TrivialReporter = new jasmine.TrivialReporter();
+
+      // Hook em up to Jasmine
+      testEnv.addReporter(TrivialReporter);
+      testEnv.addReporter(JsReporter);
+      testEnv.execute();
+    };
+
+clearTests();
+runTests();
+xx*
+ * Wait for a message from the server. If it's
+ * the correct type, run the tests, and return 
+ * the object.
+
+
+socket.on('message', function(message){ 
+
+  if (message === 'run') {
+
+//    clearTests();
+  //  runTests();
+    
+    // Remove last test from the screen and create new fresh tests
+    // document.body.removeChild(document.getElementsByTagName('div')[0]);
+    // JsReporter = new jasmine.JsApiReporter();
+    //TrivialReporter = new jasmine.TrivialReporter();
+
+    // ReRun the test!!!
+    // jasmine.getEnv().execute();
+
+    //var sendResults = function() {
+      //if (JsReporter.finished) {
+       // socket.send(JSON.stringify(JsReporter));
+      //} else {
+        // setTimeout(50, sendResults());
+     // }
+    //};
+
+  } 
+}); */
