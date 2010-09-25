7bcab52a28351dd85462992328acce547df23352
Brian Egan <brian@brianegan.com>
2010-09-24 20:37:43 -0700

    Starting from scratch with tests now that I know what I want to do

---
+++ test/hottest-test.js
@@ -0,0 +1,13 @@
+// HotTest Tests
+
+// Requires
+var vows = require('vows'),
+    assert = require('assert');
+
+var suite = vows.describe('Hot Test Unit Test Runner');
+
+suite.addBatch({
+  'Should Parse Arguments': {
+    
+  }
+});
